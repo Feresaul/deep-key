0e5b0577ea31f5ef7e9ee4e7d9cbf56ad53f1535
Esaul Franco <f.esaul.franco.r@gmail.com>
2025-05-07 18:48:18 -0600

    Add linting and testing scripts; implement tests for getKeyValue function
    - Added ESLint and Vitest scripts to package.json for linting and testing.
    - Created a new test file for the getKeyValue function, covering various scenarios including nested object access and mapped keys.

---
+++ tests/value.test.ts
@@ -0,0 +1,58 @@
+import { describe, it, expect } from 'vitest';
+
+import { getKeyValue } from '@nuc-lib/deep-key';
+
+const guy = {
+    id: 2,
+    personalInfo: { name: 'John Doe', age: 12, city: 'New York' },
+    contacts: [
+        { name: 'Jane Doe', email: 'afk@example.com' },
+        { name: 'Alice Smith', email: 'alice@example.com' },
+        { name: 'Bob Johnson', email: 'bob@example.com' },
+        { name: 'Charlie Brown', email: 'charlie@example.com' }
+    ],
+    associatedIds: [23, 43, 67, 89]
+};
+
+describe('getKeyValue', () => {
+    it('should return the value of a key in an object', () => {
+        expect(getKeyValue(guy, 'id')).toBe(guy.id);
+
+        expect(getKeyValue(guy, 'personalInfo')).toBe(guy.personalInfo);
+        expect(getKeyValue(guy, 'personalInfo.name')).toBe(
+            guy.personalInfo.name
+        );
+        expect(getKeyValue(guy, 'personalInfo.age')).toBe(guy.personalInfo.age);
+        expect(getKeyValue(guy, 'personalInfo.name')).toBe(
+            guy.personalInfo.city
+        );
+
+        guy.contacts.forEach((contact, index) => {
+            expect(getKeyValue(guy, `contacts.${index}.name`)).toBe(
+                contact.name
+            );
+            expect(getKeyValue(guy, `contacts.${index}.email`)).toBe(
+                contact.email
+            );
+        });
+
+        expect(getKeyValue(guy, 'associatedIds.2')).toBe(
+            guy.associatedIds.at(2)
+        );
+    });
+
+    it('should return the value of a key in an object - mapped keys', () => {
+        expect(getKeyValue(guy, '[contacts].name')).toEqual([
+            'Jane Doe',
+            'Alice Smith',
+            'Bob Johnson',
+            'Charlie Brown'
+        ]);
+        expect(getKeyValue(guy, '[contacts].email')).toEqual([
+            'afk@example.com',
+            'alice@example.com',
+            'bob@example.com',
+            'charlie@example.com'
+        ]);
+    });
+});
