1b9c2aeb7a2985c11a92d979617183698654b4f6
Esaul Franco <44300097+Feresaul@users.noreply.github.com>
2025-09-27 08:58:59 -0600

    Refactor sorting function to improve type handling and performance (#17)
    * Refactor sorting function to improve type handling and performance
    
    * Fix sorting logic to ensure correct order for non-numeric values

--- lib/utils/sort.ts
+++ lib/utils/sort.ts
@@ -18,9 +18,20 @@ export const sortByKeyValue = <T extends TObject>({
     key,
     order = 'ASC'
 }: SortParams<T>) => {
-    const sortedArray = array.slice().sort((a, b) => {
-        return String(getKeyValue({ object: a, key })).toLowerCase() >=
-            String(getKeyValue({ object: b, key })).toLowerCase()
+    const sortedArray = array.toSorted((a, b) => {
+        const aValue = getKeyValue({ object: a, key });
+        const bValue = getKeyValue({ object: b, key });
+
+        if (typeof aValue === 'number' && typeof bValue === 'number') {
+            return aValue - bValue;
+        }
+        if (aValue instanceof Date && bValue instanceof Date) {
+            return aValue.getTime() - bValue.getTime();
+        }
+        if (typeof aValue === 'boolean' && typeof bValue === 'boolean') {
+            return Number(aValue) - Number(bValue);
+        }
+        return String(aValue).toLowerCase() >= String(bValue).toLowerCase()
             ? 1
             : -1;
     });
