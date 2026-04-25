4ed36374c1c27f164b7fc5413190957b5ee04e68
Esaul Franco <f.esaul.franco.r@gmail.com>
2026-04-25 07:24:29 -0600

    Refactor ESLint configuration and update TypeScript peer dependency version

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,15 @@
 import js from '@eslint/js';
 import tsEslint from 'typescript-eslint';
 
-export default tsEslint.config({
-    extends: [js.configs.recommended, ...tsEslint.configs.recommended],
-    files: ['**/*.ts'],
-    rules: {
-        '@typescript-eslint/no-unused-expressions': 'off'
-    }
-});
+export default [
+    {
+        ignores: ['dist']
+    },
+    ...tsEslint.config({
+        extends: [js.configs.recommended, ...tsEslint.configs.recommended],
+        files: ['**/*.ts'],
+        rules: {
+            '@typescript-eslint/no-unused-expressions': 'off'
+        }
+    })
+];
